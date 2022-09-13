74c2fbdec0a03a07bdbd745cf781f10e83373e29
Bevis <bevisd@btinternet.com>
2022-09-13 21:37:15 +0100

    Replaced Pygame with Tkinter GUI

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pygame
+import tkinter as tk
 
 WIDTH = 600
 HEIGHT = 600
+CONTROL_WIDTH = 200
+
 MASS_COLOR = (0, 0, 255)
 SPRING_COLOR = (0, 0, 0)
-TIME_STEP = 0.005
+TIME_STEP = 0.1
 
 
 def calculate_normal_modes(T, V):
@@ -43,11 +45,93 @@ def calculate_positions(omegas, modes, coefficients, time):
     return deltas
 
 
+
+class Mass:
+    def __init__(self, canvas, x, y, r, fill="#000000", outline="#000000"):
+        self.canvas = canvas
+        self.fill = fill
+        self.outline = outline
+
+        self.id = self.canvas.create_oval(x - r, y - r,
+                                       x + r, y + r,
+                                       fill=fill, outline=outline)
+
+        self.r = r
+        self._x = x
+        self._y = y
+        self.x = x
+        self.y = y
+
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        self.canvas.moveto(self.id, value - self.r, self.y - self.r)
+        self._x = value
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        self.canvas.moveto(self.id, self.x - self.r, value - self.r)
+        self._y = value
+
+
+class App:
+    def __init__(self, main, omegas, modes, coefficients,
+                 equilib_coords, inital_coords, time=0):
+        self.main = main
+        self.omegas = omegas
+        self.modes = modes
+        self.coefficients = coefficients
+        self.equilib_coords = equilib_coords
+        self.time = time
+        self.running = True
+
+        self.canvas = tk.Canvas(self.main, width=WIDTH, height=HEIGHT,
+                                bg="white", bd=1, highlightthickness=0,
+                                relief="solid")
+        self.canvas.pack(side="left")
+
+        self.masses = [Mass(self.canvas, x * WIDTH, HEIGHT/2, 10) for x in inital_coords]
+        self.canvas.pack()
+        self.main.after(0, self.animation)
+
+    def animation(self):
+        deltas = calculate_positions(self.omegas, self.modes,
+                                     self.coefficients, self.time)
+        coords = [((self.equilib_coords[0] + deltas[0]) * WIDTH, HEIGHT // 2),
+                  ((self.equilib_coords[1] + deltas[1]) * WIDTH, HEIGHT // 2),
+                  ((self.equilib_coords[2] + deltas[2]) * WIDTH, HEIGHT // 2)]
+
+        for i in range(len(self.masses)):
+            self.masses[i].x = coords[i][0]
+
+        self.time += TIME_STEP
+
+        if self.running:
+            self.main.after(10, self.animation)
+
+
+def pause_animation():
+    if app.running:
+        app.running = False
+
+    else:
+        app.running = True
+        app.animation()
+
+
 if __name__ == "__main__":
     # Coordinates given as fractions of window width
     equilib_coords = [0.25, 0.5, 0.75]
-    initial_coords = [0.25, 0.5, 0.75]
-    initial_speeds = [0.02, -0.01, -0.01]
+    initial_coords = [0.15, 0.5, 0.85]
+    initial_speeds = [0.0, 0.0, 0.0]
 
     # Kinetic Energy Tensor
     T = np.array([[1, 0, 0],
@@ -68,34 +152,43 @@ if __name__ == "__main__":
 
     t = 0
 
-    pygame.init()
-    screen = pygame.display.set_mode([WIDTH, HEIGHT])
-    running = True
+    window = tk.Tk()
+    window.geometry(f"{WIDTH + CONTROL_WIDTH}x{HEIGHT}")
 
-    while running:
-        # PyGame Events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+    app = App(window, omegas, modes, coefficients,
+              equilib_coords, initial_coords)
 
-        # Calculate New Positions
-        deltas = calculate_positions(omegas, modes, coefficients, t)
+    pause = tk.Button(window, text="Pause", command=pause_animation)
+    pause.pack()
 
-        # PyGame Display Modes
-        screen.fill((255, 255, 255))
+    window.mainloop()
 
-        coords = [((equilib_coords[0] + deltas[0]) * WIDTH, HEIGHT // 2),
-                  ((equilib_coords[1] + deltas[1]) * WIDTH, HEIGHT // 2),
-                  ((equilib_coords[2] + deltas[2]) * WIDTH, HEIGHT // 2)]
-
-        pygame.draw.line(screen, SPRING_COLOR, coords[0], coords[1])
-        pygame.draw.line(screen, SPRING_COLOR, coords[1], coords[2])
-
-        pygame.draw.circle(screen, MASS_COLOR, (coords[0][0], coords[0][1]), 20)
-        pygame.draw.circle(screen, MASS_COLOR, (coords[1][0], coords[1][1]), 20)
-        pygame.draw.circle(screen, MASS_COLOR, (coords[2][0], coords[2][1]), 20)
-
-        pygame.display.flip()
-        t += TIME_STEP
-
-    pygame.quit()
+    # running = True
+    #
+    # while running:
+    #     # PyGame Events
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             running = False
+    #
+    #     # Calculate New Positions
+    #
+    #
+    #     # PyGame Display Modes
+    #     screen.fill((255, 255, 255))
+    #
+    #     coords = [((equilib_coords[0] + deltas[0]) * WIDTH, HEIGHT // 2),
+    #               ((equilib_coords[1] + deltas[1]) * WIDTH, HEIGHT // 2),
+    #               ((equilib_coords[2] + deltas[2]) * WIDTH, HEIGHT // 2)]
+    #
+    #     pygame.draw.line(screen, SPRING_COLOR, coords[0], coords[1])
+    #     pygame.draw.line(screen, SPRING_COLOR, coords[1], coords[2])
+    #
+    #     pygame.draw.circle(screen, MASS_COLOR, (coords[0][0], coords[0][1]), 20)
+    #     pygame.draw.circle(screen, MASS_COLOR, (coords[1][0], coords[1][1]), 20)
+    #     pygame.draw.circle(screen, MASS_COLOR, (coords[2][0], coords[2][1]), 20)
+    #
+    #     pygame.display.flip()
+    #     t += TIME_STEP
+    #
+    # pygame.quit()
