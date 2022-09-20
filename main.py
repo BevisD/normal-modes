2ad8412b91b77741563c93d180ebe1ae154535e0
Bevis <bevisd@btinternet.com>
2022-09-20 16:06:22 +0100

    Fully OOP

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class NormalModeSystem:
         self.n = n
         self.equilib_coords = [(i + 1) / (self.n + 1) for i in range(self.n)]
         self.initial_coords = [(i + 1) / (self.n + 1) for i in range(self.n)]
-        self.initial_speeds = [0.0 for i in range(self.n)]
+        self.initial_speeds = [0.0 for _ in range(self.n)]
         self.initial_speeds[0] = 0.05
         self.initial_speeds[-1] = -0.05
 
@@ -47,7 +47,7 @@ class NormalModeSystem:
         return np.round(omegas, 6), np.round(eigenvectors, 6)
 
     def calculate_coefficients(self):
-        coeffs = np.array([(0, 0) for j in self.omegas], dtype=np.float64)
+        coeffs = np.array([(0, 0) for _ in self.omegas], dtype=np.float64)
         offsets = np.subtract(self.initial_coords, self.equilib_coords)
 
         for j in range(self.n):
@@ -97,14 +97,195 @@ class NormalModeSystem:
         return phases
 
 
-def quit_callback():
-    global ended
-    ended = True
+class Pygame:
+    def __init__(self):
+        # Initialize Pygame
+        pygame.init()
+        self.screen = pygame.display.set_mode([WIDTH, HEIGHT])
+        self.paused = False
+
+    def display_masses(self, coordinates):
+        # PyGame Display Modes
+        self.screen.fill((255, 255, 255))
+
+        for i in range(N_MASSES - 1):
+            pygame.draw.line(self.screen, SPRING_COLOR,
+                             coordinates[i], coordinates[i + 1])
+
+        for coord in coords:
+            pygame.draw.circle(self.screen, MASS_COLOR, coord, 20)
+
+        pygame.display.flip()
+
+
+class GUI:
+    def __init__(self):
+        # Initialize Tkinter
+        self.root = tk.Tk()
+        self.root.geometry(f"425x{(N_MASSES + 1) * 22}")
+        self.root.protocol("WM_DELETE_WINDOW", self.quit_callback)
+        self.root.title("Normal Modes Control Panel")
+        self.main_dialog = tk.Frame(self.root)
+
+        self.update_in_progress = False
+
+        # Create Position Inputs and Labels
+        self.position_variables = [tk.DoubleVar() for _ in range(N_MASSES)]
+        self.position_entries = [tk.Entry(self.main_dialog, width=6,
+                                          textvariable=self.position_variables[i])
+                                 for i in range(N_MASSES)]
+        self.position_labels = [tk.Label(self.main_dialog, text=f"x_{i}")
+                                for i in range(N_MASSES)]
+
+        # Create Velocity Inputs and Labels
+        self.velocity_variables = [tk.DoubleVar() for _ in range(N_MASSES)]
+        self.velocity_entries = [tk.Entry(self.main_dialog, width=6,
+                                          textvariable=self.velocity_variables[i])
+                                 for i in range(N_MASSES)]
+        self.velocity_labels = [tk.Label(self.main_dialog, text=f"v_{i}")
+                                for i in range(N_MASSES)]
+
+        # Create Mode Amplitude Inputs and Labels
+        self.amplitude_variables = [tk.DoubleVar() for _ in range(N_MASSES)]
+        self.amplitude_labels = [tk.Label(self.main_dialog, text=f"Mode {i}: Amplitude")
+                                 for i in range(N_MASSES)]
+        self.amplitude_entries = [tk.Entry(self.main_dialog, width=6,
+                                           textvariable=self.amplitude_variables[i])
+                                  for i in range(N_MASSES)]
+
+        # Create Mode Phase Inputs and Labels
+        self.phase_variables = [tk.DoubleVar() for _ in range(N_MASSES)]
+        self.phase_labels = [tk.Label(self.main_dialog, text=f"Mode {i}: Phase")
+                             for i in range(N_MASSES)]
+        self.phase_entries = [tk.Entry(self.main_dialog, width=6,
+                                       textvariable=self.phase_variables[i])
+                              for i in range(N_MASSES)]
+
+        # Create and Place Pause Button
+        self.pause_button = tk.Button(self.root, text="Pause", command=toggle_pause)
+
+        phases = modes.coefficients_to_phases()
+        self.display_mode_phases(phases)
+
+        for i in range(N_MASSES):
+            start_row = 1
+            # Place Position Elements
+            self.position_labels[i].grid(row=start_row + i, column=0)
+            self.position_entries[i].grid(row=start_row + i, column=1)
+
+            # Place Velocity Elements
+            self.velocity_labels[i].grid(row=start_row + i, column=2)
+            self.velocity_entries[i].grid(row=start_row + i, column=3)
+
+            self.amplitude_labels[i].grid(row=start_row + i, column=4)
+            self.amplitude_entries[i].grid(row=start_row + i, column=5)
+
+            self.phase_labels[i].grid(row=start_row + i, column=6)
+            self.phase_entries[i].grid(row=start_row + i, column=7)
+
+            self.position_variables[i].set(modes.initial_coords[i])
+            self.velocity_variables[i].set(modes.initial_speeds[i])
+
+            self.position_variables[i].trace_add("write", self.set_position_function(i))
+            self.velocity_variables[i].trace_add("write", self.set_velocity_function(i))
+            self.amplitude_variables[i].trace_add("write", self.set_mode_function(i))
+            self.phase_variables[i].trace_add("write", self.set_mode_function(i))
+
+            self.phase_entries[i].config(state=tk.DISABLED)
+            self.amplitude_entries[i].config(state=tk.DISABLED)
+
+        self.main_dialog.pack(fill=tk.BOTH, expand=True)
+        self.pause_button.pack(side=tk.LEFT)
+        self.ended = False
+
+    def quit_callback(self):
+        self.ended = True
+
+    def set_position_function(self, id):
+        def set_position(name, index, mode):
+            if self.update_in_progress or not game.paused:
+                return
+
+            if modes.t != 0:
+                modes.initial_coords = modes.current_coords
+
+            try:
+                modes.initial_coords[id] = self.position_variables[id].get()
+            except:
+                pass
+
+            modes.initial_speeds = modes.current_speeds
+
+            modes.t = 0
+            modes.coefficients = modes.calculate_coefficients()
+
+            phases = modes.coefficients_to_phases()
+
+            self.update_in_progress = True
+            self.display_mode_phases(phases)
+            self.update_in_progress = False
+
+        return set_position
+
+    def set_velocity_function(self, id):
+        def set_velocity(name, index, mode):
+            if self.update_in_progress or not game.paused:
+                return
+
+            if modes.t != 0:
+                modes.initial_coords = modes.current_coords
+                modes.initial_speeds = modes.current_speeds
+
+            try:
+                modes.initial_speeds[id] = self.velocity_variables[id].get()
+            except:
+                pass
+
+            modes.t = 0
+            modes.coefficients = modes.calculate_coefficients()
+
+            phases = modes.coefficients_to_phases()
+
+            self.update_in_progress = True
+            self.display_mode_phases(phases)
+            self.update_in_progress = False
+
+        return set_velocity
+
+    def set_mode_function(self, id):
+        def set_mode(name, index, mode):
+            if self.update_in_progress or not game.paused:
+                return
+
+            try:
+                modes.t = 0
+                phase = np.deg2rad(self.phase_variables[id].get())
+                amplitude = self.amplitude_variables[id].get() / 100
+
+                modes.coefficients[id][0] = amplitude * np.cos(phase)
+                modes.coefficients[id][1] = amplitude * np.sin(phase)
+
+                deltas = modes.calculate_positions()
+                speeds = modes.calculate_velocities()
+
+                self.update_in_progress = True
+                for i in range(N_MASSES):
+                    self.position_variables[i].set(round(deltas[i], 3))
+                    self.velocity_variables[i].set(round(speeds[i], 3))
+                self.update_in_progress = False
+            except:
+                pass
+
+        return set_mode
+
+    def display_mode_phases(self, phases):
+        for i in range(N_MASSES):
+            self.phase_variables[i].set(phases[i][0])
+            self.amplitude_variables[i].set(round(100 * phases[i][1], 3))
 
 
 def toggle_pause():
-    global paused
-    if paused:
+    if game.paused:
         state = tk.DISABLED
         message = "Pause"
     else:
@@ -112,185 +293,22 @@ def toggle_pause():
         message = "Play"
 
     for i in range(N_MASSES):
-        amplitude_entries[i].config(state=state)
-        phase_entries[i].config(state=state)
+        gui.amplitude_entries[i].config(state=state)
+        gui.phase_entries[i].config(state=state)
 
-    pause_button.config(text=message)
-    paused = not paused
-
-
-def set_position_function(id):
-    def set_position(name, index, mode):
-        global update_in_progress
-        if update_in_progress or not paused:
-            return
-
-        if modes.t != 0:
-            modes.initial_coords = modes.current_coords
-
-        try:
-            modes.initial_coords[id] = position_variables[id].get()
-        except:
-            pass
-
-        modes.initial_speeds = modes.current_speeds
-
-        modes.t = 0
-        modes.coefficients = modes.calculate_coefficients()
-
-        phases = modes.coefficients_to_phases()
-
-        update_in_progress = True
-        display_mode_phases(phases)
-        update_in_progress = False
-    return set_position
-
-
-def set_velocity_function(id):
-    def set_velocity(name, index, mode):
-        global update_in_progress
-        if update_in_progress or not paused:
-            return
-
-        if modes.t != 0:
-            modes.initial_coords = modes.current_coords
-            modes.initial_speeds = modes.current_speeds
-
-        try:
-            modes.initial_speeds[id] = velocity_variables[id].get()
-        except:
-            pass
-
-        modes.t = 0
-        modes.coefficients = modes.calculate_coefficients()
-
-        phases = modes.coefficients_to_phases()
-
-        update_in_progress = True
-        display_mode_phases(phases)
-        update_in_progress = False
-    return set_velocity
-
-
-def set_mode_function(id):
-    def set_mode(name, index, mode):
-        global update_in_progress
-        if update_in_progress or not paused:
-            return
-
-        try:
-            modes.t = 0
-            phase = np.deg2rad(phase_variables[id].get())
-            amplitude = amplitude_variables[id].get() / 100
-
-            modes.coefficients[id][0] = amplitude * np.cos(phase)
-            modes.coefficients[id][1] = amplitude * np.sin(phase)
-
-            deltas = modes.calculate_positions()
-            speeds = modes.calculate_velocities()
-
-            update_in_progress = True
-            for i in range(N_MASSES):
-                position_variables[i].set(round(deltas[i], 3))
-                velocity_variables[i].set(round(speeds[i], 3))
-            update_in_progress = False
-        except:
-            pass
-    return set_mode
-
-
-def display_mode_phases(phases):
-    for i in range(N_MASSES):
-        phase_variables[i].set(phases[i][0])
-        amplitude_variables[i].set(round(100 * phases[i][1], 3))
+    gui.pause_button.config(text=message)
+    game.paused = not game.paused
 
 
 if __name__ == "__main__":
     modes = NormalModeSystem(N_MASSES)
+    game = Pygame()
+    gui = GUI()
 
-    # Initialize Pygame
-    pygame.init()
-    screen = pygame.display.set_mode([WIDTH, HEIGHT])
-    paused = False
-
-    # Initialize Tkinter
-    root = tk.Tk()
-    root.geometry("500x200")
-    root.protocol("WM_DELETE_WINDOW", quit_callback)
-    root.title("Normal Modes Control Panel")
-    main_dialog = tk.Frame(root)
-
-    # Create Position Inputs and Labels
-    position_variables = [tk.DoubleVar() for i in range(N_MASSES)]
-    position_entries = [tk.Entry(main_dialog, width=6,
-                                 textvariable=position_variables[i])
-                        for i in range(N_MASSES)]
-    position_labels = [tk.Label(main_dialog, text=f"x_{i}")
-                       for i in range(N_MASSES)]
-
-    # Create Velocity Inputs and Labels
-    velocity_variables = [tk.DoubleVar() for i in range(N_MASSES)]
-    velocity_entries = [tk.Entry(main_dialog, width=6,
-                                 textvariable=velocity_variables[i])
-                        for i in range(N_MASSES)]
-    velocity_labels = [tk.Label(main_dialog, text=f"v_{i}")
-                       for i in range(N_MASSES)]
-
-    # Create Mode Amplitude Inputs and Labels
-    amplitude_variables = [tk.DoubleVar() for i in range(N_MASSES)]
-    amplitude_labels = [tk.Label(main_dialog, text=f"Mode {i}: Amplitude") for i in range(N_MASSES)]
-    amplitude_entries = [tk.Entry(main_dialog, width=6,
-                                  textvariable=amplitude_variables[i])
-                         for i in range(N_MASSES)]
-
-    # Create Mode Phase Inputs and Labels
-    phase_variables = [tk.DoubleVar() for i in range(N_MASSES)]
-    phase_labels = [tk.Label(main_dialog, text=f"Mode {i}: Phase") for i in range(N_MASSES)]
-    phase_entries = [tk.Entry(main_dialog, width=6,
-                              textvariable=phase_variables[i])
-                     for i in range(N_MASSES)]
-
-    # Create and Place Pause Button
-    pause_button = tk.Button(root, text="Pause", command=toggle_pause)
-
-    phases = modes.coefficients_to_phases()
-    display_mode_phases(phases)
-
-    for i in range(N_MASSES):
-        start_row = 1
-        # Place Position Elements
-        position_labels[i].grid(row=start_row + i, column=0)
-        position_entries[i].grid(row=start_row + i, column=1)
-
-        # Place Velocity Elements
-        velocity_labels[i].grid(row=start_row + i, column=2)
-        velocity_entries[i].grid(row=start_row + i, column=3)
-
-        amplitude_labels[i].grid(row=start_row + i, column=4)
-        amplitude_entries[i].grid(row=start_row + i, column=5)
-
-        phase_labels[i].grid(row=start_row + i, column=6)
-        phase_entries[i].grid(row=start_row + i, column=7)
-
-        position_variables[i].set(modes.initial_coords[i])
-        velocity_variables[i].set(modes.initial_speeds[i])
-
-        position_variables[i].trace_add("write", set_position_function(i))
-        velocity_variables[i].trace_add("write", set_velocity_function(i))
-        amplitude_variables[i].trace_add("write", set_mode_function(i))
-        phase_variables[i].trace_add("write", set_mode_function(i))
-
-        phase_entries[i].config(state=tk.DISABLED)
-        amplitude_entries[i].config(state=tk.DISABLED)
-
-    main_dialog.pack(fill=tk.BOTH, expand=True)
-    pause_button.pack(side=tk.LEFT)
-    ended = False
-
-    while not ended:
+    while not gui.ended:
         # Tkinter Events
         try:
-            main_dialog.update()
+            gui.main_dialog.update()
         except:
             print("Dialog Error")
 
@@ -299,7 +317,7 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 ended = True
 
-        if not paused:
+        if not game.paused:
             modes.t += TIME_STEP
 
         # Calculate New Positions
@@ -309,21 +327,12 @@ if __name__ == "__main__":
         coords = [(positions[i] * WIDTH, HEIGHT // 2)
                   for i in range(N_MASSES)]
 
-        if not paused:
+        game.display_masses(coords)
+
+        if not game.paused:
             for i in range(N_MASSES):
-                position_variables[i].set(round(coords[i][0] / WIDTH, 3))
-                velocity_variables[i].set(round(velocities[i], 3))
-
-        # PyGame Display Modes
-        screen.fill((255, 255, 255))
-
-        for i in range(N_MASSES - 1):
-            pygame.draw.line(screen, SPRING_COLOR, coords[i], coords[i + 1])
-
-        for coord in coords:
-            pygame.draw.circle(screen, MASS_COLOR, coord, 20)
-
-        pygame.display.flip()
+                gui.position_variables[i].set(round(coords[i][0] / WIDTH, 3))
+                gui.velocity_variables[i].set(round(velocities[i], 3))
 
     pygame.quit()
-    main_dialog.destroy()
+    gui.main_dialog.destroy()
